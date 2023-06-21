eaadbbbd57f3ec8e252f201e643842c8661083b3
murilonicemento <murilo_nascimento22@outlook.com>
2023-06-21 13:52:10 -0300

    style: estilização header

---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,16 @@
+import { MyHeader } from "./styled";
+
+export function Header() {
+  return (
+    <MyHeader>
+      <nav>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </nav>
+    </MyHeader>
+  );
+}
---
+++ src/pages/Header/styled.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import bgSidebarMobile from "../../assets/images/bg-sidebar-mobile.svg";
+import bgSidebarDesktop from "../../assets/images/bg-sidebar-desktop.svg";
+import { colors } from "../../config/colors";
+
+export const MyHeader = styled.header`
+  width: 100%;
+  height: 200px;
+  background-image: url(${bgSidebarMobile});
+  background-size: cover;
+  padding: 30px;
+
+  nav {
+    display: grid;
+    place-items: center;
+    ul {
+      display: flex;
+      gap: 20px;
+
+      li {
+        display: grid;
+        place-items: center;
+        width: 35px;
+        height: 35px;
+        list-style-type: none;
+        font-weight: 600;
+        color: ${colors.neutral.magnolia};
+        border: 1px solid ${colors.neutral.magnolia};
+        border-radius: 50%;
+        cursor: pointer;
+        transition: all 0.2s ease;
+
+        &:hover {
+          background-color: ${colors.primary.lightBlue};
+          color: ${colors.primary.marineBlue};
+          transition: all 0.2s ease;
+        }
+      }
+    }
+  }
+`;
